8c57a53f62ef0f13a2a1675cc3079c8b8b093ff0
thiagomag <thiagomagdalena@gmail.com>
2025-03-25 20:58:53 -0300

    correção

--- src/main/java/br/com/postechfiap/restaurantreservationapi/dto/restaurante/RestauranteRequest.java
+++ src/main/java/br/com/postechfiap/restaurantreservationapi/dto/restaurante/RestauranteRequest.java
@@ -2,6 +2,7 @@ package br.com.postechfiap.restaurantreservationapi.dto.restaurante;
 
 import br.com.postechfiap.restaurantreservationapi.dto.endereco.EnderecoRequest;
 import br.com.postechfiap.restaurantreservationapi.enuns.TiposCozinhaEnum;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -29,6 +30,7 @@ import lombok.ToString;
 public class RestauranteRequest {
 
     @Schema(description = "Identificador do restaurante", example = "1")
+    @JsonIgnore
     private Long id;
 
     @NotBlank(message = "O nome do restaurante é obrigatório")
--- src/main/java/br/com/postechfiap/restaurantreservationapi/dto/usuario/UsuarioRequest.java
+++ src/main/java/br/com/postechfiap/restaurantreservationapi/dto/usuario/UsuarioRequest.java
@@ -1,8 +1,10 @@
 package br.com.postechfiap.restaurantreservationapi.dto.usuario;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -18,13 +20,18 @@ import lombok.*;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UsuarioRequest {
 
+    @Schema(description = "ID do usuário", example = "1")
+    @JsonIgnore
     private Long id;
+    @Schema(description = "Nome do usuário", example = "João da Silva")
     @NotNull
     @NotBlank
     private String nome;
+    @Schema(description = "Email do usuário", example = "email@email.com")
     @NotNull
     @NotBlank
     private String email;
+    @Schema(description = "Telefone do usuário", example = "11999999999")
     private String telefone;
 
 }
